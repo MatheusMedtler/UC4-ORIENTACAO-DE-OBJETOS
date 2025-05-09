11b494c5f3e7309c014fca0bc383ac941585706c
Matheus Medtler Alves <ma284163@gmail.com>
2025-05-08 21:51:41 -0300

    Add files via upload

---
+++ UC4-6/Flow/cat.ts
@@ -0,0 +1,22 @@
+/*import { Anima } from "./animal";
+
+
+export class cat extends Anima{
+
+    constructor(name:string,weight:number){
+        super(name, weight)
+    }
+
+    meow():void{
+        console.log("Meow meow")
+    }
+}
+
+function realizarAcao(qualquerAnimal:Anima){
+    qualquerAnimal.eat(10);
+
+}
+
+let meuGato = new cat("Daeny",7)
+let meuAnimal = new cat("Animal1",7)
+realizarAcao(meuAnimal)*/
